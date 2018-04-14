22ea13bbd24dffa1d8fa2e0a62865444e0458907
NuclearWinter <klugewilliam@gmail.com>
2018-04-13 19:33:38 -0500

    Tone analyzer demo done

---
+++ motherzuccingcode/src/test/java/ToneAnalyzing.java
@@ -0,0 +1,24 @@
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneOptions;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class ToneAnalyzing {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        ToneAnalyzer toneAnalyzer = new ToneAnalyzer(
+                "2018-4-13",
+                dotenv.get("WATSON_USERNAME"),
+                dotenv.get("WATSON_PASSWORD"));
+
+        String text = "Damion?! More like LAMEion! haha yeet gotem!";
+
+        ToneOptions toneOptions = new ToneOptions.Builder().text(text).build();
+        ToneAnalysis tone = toneAnalyzer.tone(toneOptions).execute();
+        System.out.println(tone);
+
+    }
+
+}
