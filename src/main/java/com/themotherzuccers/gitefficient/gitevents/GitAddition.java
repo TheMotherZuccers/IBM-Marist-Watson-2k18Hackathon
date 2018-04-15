cc54555995dcf033edd6268645ce37b8bec2a014
dang024 <dangosimpson@gmail.com>
2018-04-14 20:57:38 -0400

    Deleted GitAddition and GitSubtraction :/

--- src/main/java/com/themotherzuccers/gitefficient/gitevents/GitAddition.java
+++
@@ -1,5 +0,0 @@
-package com.themotherzuccers.gitefficient.gitevents;
-
-//Checks for code added in git commits
-class GitAddition extends GitModification {
-}
--- src/main/java/com/themotherzuccers/gitefficient/gitevents/GitSubtraction.java
+++
@@ -1,6 +0,0 @@
-package com.themotherzuccers.gitefficient.gitevents;
-
-//Checks for code that was removed in git commit, wasted code
-class GitSubtraction extends GitModification {
-
-}
